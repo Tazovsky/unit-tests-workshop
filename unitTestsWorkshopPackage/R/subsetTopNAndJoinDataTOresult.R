2242f9d5339bd4bea2cfecaa13796434e579f816
Kamil Foltynski <kamil@foltynski.com>
2019-04-04 18:15:15 +0200

    example function added

---
+++ unitTestsWorkshopPackage/R/subsetTopNAndJoinDataTOresult.R
@@ -0,0 +1,32 @@
+#' subsetTopNAndJoinDataTOresult
+#'
+#' Subsets first \code{top_n_rows} rows of data.frame in provided \code{list_data}
+#' and then join `data2join` to every element in that list by sepcified \code{key}
+#' 
+#' @param list_data list containing data.frames
+#' @param data2join 
+#' @param key 
+#' @param top_n_rows 
+#'
+#' @return list
+#' @export
+#'
+subsetTopNAndJoinDataTOresult <- function(list_data, data2join, key, top_n_rows = 3) {
+  
+  subsetTopN <- function(input, n) {
+    lapply(input, function(x) {
+      
+      if (!is.data.frame(x))
+        stop(sprintf("'x' argument is not data.frame class but %s", class(x)))
+      
+      head(x, n)
+    })
+  }
+  
+  result <- subsetTopN(list_data, top_n_rows)
+  
+  # join horse power to each element of list
+  lapply(result, function(x) {
+    x %>% left_join(data2join, by = key)
+  })
+}
