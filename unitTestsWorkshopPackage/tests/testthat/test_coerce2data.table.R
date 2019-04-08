23c15b6337c58750b227cea33f9c94017adb9c63
Kamil Foltynski <kamil@foltynski.com>
2019-04-08 22:47:36 +0200

    test for coerce2data.table

---
+++ unitTestsWorkshopPackage/tests/testthat/test_coerce2data.table.R
@@ -0,0 +1,28 @@
+testthat::context("coerce2data.table")
+
+testthat::test_that("output equals expected output", {
+  
+  if (FALSE) {
+    # save expected output
+    expected_data <- unitTestsWorkshopPackage::coerce2data.table(head(iris))
+    saveRDS(expected_data, file = "expected_output.RDS")
+  }
+  
+  expected_data <- readRDS("expected_output.RDS")
+  
+  testthat::expect_identical(unitTestsWorkshopPackage::coerce2data.table(head(iris)), 
+                             expected_data)
+  
+  
+  # error should be thrown: Objects equal but not identical
+  testthat::expect_identical(unitTestsWorkshopPackage::coerce2data.table(head(iris)), expected_data)
+  testthat::expect_equal(unitTestsWorkshopPackage::coerce2data.table(head(iris)), expected_data)
+  
+  str(expected_data)
+  
+  testthat::expect_identical(
+    attributes(expected_data)$.internal.selfref,
+    attributes(unitTestsWorkshopPackage::coerce2data.table(head(iris)))$.internal.selfref
+  )
+  
+})
